07c1b8c1bbe0806fa43ef72115c3f5b3c8fad9ee
KRISHNA BABGAL <krishna.bagal@gmail.com>
2018-12-11 16:25:57 +0530

    some changes in file

--- greplogs.sh
+++ greplogs.sh
@@ -1,29 +1,36 @@
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
-#Script Name: greplogs.sh
-#Date: Dec 18th, 2017. 
-#Modified: NA
-#Versioning: NA
-#Owner: Krishna Bagal.
-#Info: Check string in logs and send an alert to email address. 
+# Script Name: greplogs.sh
+# Date: Dec 18th, 2017. 
+# Modified: NA
+# Versioning: NA
+# Owner: Krishna Bagal.
+# Info: Check string in logs and send an alert to email address. 
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
-hostname=`hostname`
-logFileLocation=`<Tomcat Directory Location>`
-mailFileLoc="/tmp/tomcat.log"
-curhour=$(date +%a" "%b" "%d" "%H:%M)
-last5min=$(date +%a" "%b" "%d" "%H:%M -d  "5 min ago")
-fileName="catalina.out"
-filePath=$logFileLocation/logs/$fileName
-cc="krishna.bagal@gmail.com"
+HOSTNAME=`hostname -f`
+HOSTIP=`ip route get 1 | awk '{print $NF;exit}'`
+MAILFILELOCATION="/tmp/tomcat.log"
+CURRENTHOUR=$(date +%a" "%b" "%d" "%H:%M)
+LASTFIVEMIN=$(date +%a" "%b" "%d" "%H:%M -d "5 min ago")
+FILENAME="catalina.out"
+DIRPATH="<TOMCAT-DIRECTORY>"
+FILEPATH="$DIRPATH/logs/$FILENAME"
+EXCEPTIONSTRING="java.lang.OutOfMemoryError"
+TO="root@krishnabagal.com"
+SED="/bin/sed"
+
+# Clean old mail logs.
+cat /dev/null > $MAILFILELOCATION
+
+# Filter last/old 5 min logs from file.
+$SED -n "/$last5min/ , /$curhour/p" $FILEPATH > $MAILFILELOCATION
  
-cat $filePath |sed -n "/$last5min/ , /$curhour/p" >$mailFileLoc
- 
-cat $mailFileLoc |egrep -i "<string>"
+cat $MAILFILELOCATION |egrep -i "$EXCEPTIONSTRING"
  
 if [ $? -eq 0 ];
 then
-        mail -s "CRITICAL: $hostname: String found in logs" $cc < $mailFileLoc
+        mail -s "CRITICAL: $HOSTNAME: $EXCEPTIONSTRING String found in logs" $TO < $MAILFILELOCATION
         exit 2
 else
-        echo "OK: String not found in log."
+        echo "OK: $EXCEPTIONSTRING String not found in log."
         exit 0
 fi
